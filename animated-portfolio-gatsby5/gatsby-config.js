576e5907d0f0a30b7205ec4223e0e9baab262ff5
RAJA KARTHIK P <rajakarthik.clg@gmail.com>
2024-10-24 21:44:52 +0530

    Fixed vulnerabilities and updated dependencies

---
+++ animated-portfolio-gatsby5/gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: "Animated Portfolio",
+    description: "A Gatsby portfolio site",
+    author: "PALAKURTHI RAJA KARTHIK", // Add this line
+  },
+  plugins: [
+    `gatsby-plugin-postcss`,
+    // other plugins
+  ],
+};
